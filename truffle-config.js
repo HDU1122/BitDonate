b381996e476795faf738fea4dd4a9fdd8b94fbc9
Hongzhe Du <hongzhedu@ucla.edu>
2022-04-17 09:08:34 -0700

    bug fixes

--- truffle-config.js
+++ truffle-config.js
@@ -11,20 +11,20 @@ module.exports = {
   networks: {
 
 
-    ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, "https://BitFund:1234aaAA@apis.ankr.com/c48d86d29d2e4e619a65e0fd75ac2942/8d7dca9bc7ae0272d841c0c2f35f8ea9/eth/fast/ropsten"),
-      network_id: 3, // network's Chain id
-      gas: 4612388, // Chain has a lower block limit than mainnet
-      confirmations: 2, // # of confs to wait between deployments. (default: 0)
-      networkCheckTimeout: 1000000,
-      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-  },
-    // development: {
-    //   host: "127.0.0.1",     // Ganache RPC server host
-    //   port: 7545,            // Ganache RPC server port
-    //   network_id: 5777,      // Ganache network ID
-    // },
+  //   ropsten: {
+  //     provider: () => new HDWalletProvider(mnemonic, "https://BitFund:1234aaAA@apis.ankr.com/c48d86d29d2e4e619a65e0fd75ac2942/8d7dca9bc7ae0272d841c0c2f35f8ea9/eth/fast/ropsten"),
+  //     network_id: 3, // network's Chain id
+  //     gas: 4612388, // Chain has a lower block limit than mainnet
+  //     confirmations: 2, // # of confs to wait between deployments. (default: 0)
+  //     networkCheckTimeout: 1000000,
+  //     timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+  //     skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+  // },
+    development: {
+      host: "127.0.0.1",     // Ganache RPC server host
+      port: 7545,            // Ganache RPC server port
+      network_id: 5777,      // Ganache network ID
+    },
 
   },
   compilers: {
